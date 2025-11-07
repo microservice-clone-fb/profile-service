de17fbc41b407e5a25f9eedb29794bcf48f2aea5
TamMai21 <maichitam185@gmail.com>
2025-11-07 12:49:56 +0700

    sync dev successfully with production

--- src/main/java/com/tam/profile/repository/UserProfileRepository.java
+++ src/main/java/com/tam/profile/repository/UserProfileRepository.java
@@ -3,6 +3,7 @@ package com.tam.profile.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tam.profile.entity.UserProfile;
@@ -14,5 +15,6 @@ public interface UserProfileRepository extends MongoRepository<UserProfile, Stri
     // List<UserProfile> findAllByUsernameLike(String username);
     Optional<UserProfile> findByContactInfoEmail(String email);
 
+    @Query("{ $or: [ { 'contactInfo.email': ?0 }, { 'contactInfo.phoneNumber': ?0 } ] }")
     Optional<UserProfile> findByEmailOrPhoneNumber(String searchString);
 }
